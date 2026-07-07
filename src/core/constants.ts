5bccae314b2ea43ca7251316bda071f4db28eec5
hokupod <hokupod@outlook.com>
2026-07-07 18:21:39 +0900

    build: bump version to 0.3.0

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -4,4 +4,4 @@ export const DEFAULT_MAX_DIFF_BYTES = 300_000;
 export const RAW_OUTPUT_MAX_CHARS = 16_384;
 export const TRACE_DIR = ".kyoso/traces";
 export const KYOSO_CHILD_AGENT = "KYOSO_CHILD_AGENT";
-export const KYOSO_VERSION = "0.1.0";
+export const KYOSO_VERSION = "0.3.0";
